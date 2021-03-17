784bba4ed0a3ff6b166875e614868972a783161c
joeljunnila <joel.junnila@gmail.com>
2021-03-17 15:28:56 +0200

    Valmistautumis activityt valmiina //edellinen tapa

---
+++ AmbuApp/app/src/main/java/com/example/ambuapp/valmActivity2.java
@@ -0,0 +1,14 @@
+package com.example.ambuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class valmActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valm2);
+    }
+}
