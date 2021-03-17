9c89ea7cc0d7417f7afd791af7b7963309b1cb63
Joonas <joonas_partanen@hotmail.com>
2021-03-17 14:32:38 +0200

    Erikoistilanteet valikko ja alanuolet

---
+++ AmbuApp/app/src/main/java/com/example/ambuapp/Erikoistilanteet.java
@@ -0,0 +1,14 @@
+package com.example.ambuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Erikoistilanteet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_erikoistilanteet);
+    }
+}
