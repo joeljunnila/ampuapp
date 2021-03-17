7b36ccafc00fdf390784c0e7f2477caabf072b03
Tempputsuki <temppi166@gmail.com>
2021-03-17 17:26:02 +0200

    laitettu tekstit taulukkoon :D

--- AmbuApp/app/src/main/java/com/example/ambuapp/Aikana1.java
+++ AmbuApp/app/src/main/java/com/example/ambuapp/Aikana1.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Aikana1 extends AppCompatActivity {
 
@@ -18,7 +19,11 @@ public class Aikana1 extends AppCompatActivity {
             "Supistuksia tulee 1-2 minuutin välein ja ne kestävät yleensä minuutin ajan\n" +
             "\n", "Yleensä lapsen pää eli tarjoutuva osa painaa välilihaa, ja emättimen ulkosuu pullottaa  ja alkaa avautua jolloin lapsen pää saattaa näkyä\n" +
             "Kun synnytys on edennyt tähän vaiheeseen, niin sitä ei voi enää estää millään tavoin\n" +
-            "Muista toimia rauhallisesti!\n", "Välilihan repeäminen estetään tukemalla sitä toisen kämmenellä, peukalo ja etusormi kurovat välilihaan joustoa "};
+            "Muista toimia rauhallisesti!\n", "Välilihan repeäminen estetään tukemalla sitä toisen kämmenellä, peukalo ja etusormi kurovat välilihaan joustoa ",
+            "Samalla toisella kädellä kontrolloidaan pään syntymistä ","Kiiretilanteessa tukeminen on tärkeämpää kuin aseptiset toimenpiteet.    Pään synnyttyä,"+
+            "se tekee tavallisesti käännöksen niin, että kasvot näyttävät sivulle\n","Pään synnyttyä, hartiat voi auttaa ulos ilman supistusta tai vaihtoehtoisesti synnyttäjän ponnistaessa seuraavan supistuksen aikana ",
+    " Molemmilla käsillä otetaan lapsen pään sivuista kiinni ja painetaan päätä alaspäin, jolloin ylempi hartia syntyy","Seuraavaksi päätä kohotetaan ylöspäin kohti häpyliitosta alemman hartian synnyttämiseksi",
+    "Kun pää ja hartiat ovat syntyneet, molempien käsien etusormet viedään lapsen selän kautta kainaloihin ja lapsen vartalo syntyy hellästi nostamalla","Jos hartiat eivät synny helposti, asetetaan jokin koroke tai tyyny tai vastaava äidin pakaroiden alle"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +39,10 @@ public class Aikana1 extends AppCompatActivity {
             public void onClick(View view) {
                 modifier++;
                 TextToChange.setText(Texts[modifier]);
-                if (modifier == 2)
+                if (modifier == 9)
                     rightArrow.setVisibility(View.INVISIBLE);
                     leftArrow.setVisibility(View.VISIBLE);
-
             }
-
-
-
         });
 
         leftArrow.setOnClickListener(new View.OnClickListener() {
@@ -55,16 +56,5 @@ public class Aikana1 extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
-
-
-
     }
-
-
-
 }
