b94387b4dda8060728e19848f7f2fde1aab95d46
Tempputsuki <temppi166@gmail.com>
2021-03-17 16:45:15 +0200

    Luotu left ja right arroweille funktiot texts taulukon pyörittämiseen

--- AmbuApp/app/src/main/java/com/example/ambuapp/Aikana1.java
+++ AmbuApp/app/src/main/java/com/example/ambuapp/Aikana1.java
@@ -11,27 +11,54 @@ import android.widget.ImageButton;
 public class Aikana1 extends AppCompatActivity {
 
     ImageButton rightArrow;
+    ImageButton leftArrow;
     TextView TextToChange;
+    int modifier = 0;
+    String[] Texts = {"Synnyttäjä kokee voimakasta ponnistamisen tarvetta, kun kohdunsuu on täysin avautunut ja sikiön pää on laskeutunut synnytyskanavan alaosaan\n" +
+            "Supistuksia tulee 1-2 minuutin välein ja ne kestävät yleensä minuutin ajan\n" +
+            "\n", "Yleensä lapsen pää eli tarjoutuva osa painaa välilihaa, ja emättimen ulkosuu pullottaa  ja alkaa avautua jolloin lapsen pää saattaa näkyä\n" +
+            "Kun synnytys on edennyt tähän vaiheeseen, niin sitä ei voi enää estää millään tavoin\n" +
+            "Muista toimia rauhallisesti!\n", "Välilihan repeäminen estetään tukemalla sitä toisen kämmenellä, peukalo ja etusormi kurovat välilihaan joustoa "};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aikana1);
 
         rightArrow = findViewById(R.id.rightArrow);
+        leftArrow = findViewById(R.id.leftArrow);
         TextToChange = findViewById(R.id.textViewSA1);
+
         rightArrow.setOnClickListener(new View.OnClickListener() {
             @Override
+            public void onClick(View view) {
+                modifier++;
+                TextToChange.setText(Texts[modifier]);
+                if (modifier == 2)
+                    rightArrow.setVisibility(View.INVISIBLE);
+                else
+                    rightArrow.setVisibility(View.VISIBLE);
+
+            }
+
+
+        });
+        leftArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                TextToChange.setText("jees");
+                modifier--;
+                if (modifier == 0)
+                    leftArrow.setVisibility(View.INVISIBLE);
+                else
+                    leftArrow.setVisibility(View.VISIBLE);
+
+
+
+                TextToChange.setText(Texts[modifier]);
+
             }
         });
 
-
-
-
-    }
-    public void textChange()
-    {
 
 
     }
