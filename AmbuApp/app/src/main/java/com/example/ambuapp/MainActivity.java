9b3c679cc2d011a9626242559cb23de8debff251
Thien <thien_@hotmail.fi>
2021-03-18 13:50:20 +0200

    little fix

--- AmbuApp/app/src/main/java/com/example/ambuapp/MainActivity.java
+++ AmbuApp/app/src/main/java/com/example/ambuapp/MainActivity.java
@@ -127,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void erikoistilanteetPage() {
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
