e8885760ba4a5e4f8bccce964be8854f248d5349
junjols <joel.junnila@gmail.com>
2021-03-18 15:47:44 +0200

    thienin ja joelin branchit yhdistetty

--- AmbuApp/app/src/main/java/com/example/ambuapp/TextViewActivity.java
+++ AmbuApp/app/src/main/java/com/example/ambuapp/TextViewActivity.java
@@ -5,19 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-<<<<<<< HEAD
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class TextViewActivity extends AppCompatActivity {
-
-
+    TextView textView;
+    String activityName;
     ImageButton rightArrow;
     ImageButton leftArrow;
     ImageButton homeButton;
-    TextView TextToChange;
+
     int modifier = 0;
-    String[] Texts = {"Synnytystehtävän (791 A-D) tulessa huomioi seuraavat asiat:  \n" +
+    String[] valmistautuminen = {"Synnytystehtävän (791 A-D) tulessa huomioi seuraavat asiat:  \n" +
             "\n" +
             "Kirjaa ylös synnyttäjän perustiedot (sairaudet, allergiat, monesko raskaus ja synnytys jne.) \n" +
             "\n" +
@@ -46,36 +45,18 @@ public class TextViewActivity extends AppCompatActivity {
             "Seuraa supistusten väliä \n" +
             "Jos synnyttäjällä tulee paineen tunne tai ponnistamisen pakko matkalla, pysäytä auto \n" +
             "Lääkkeettömiä kivunlievitys keinoja voi kokeilla kuten hierontaa tai lämpöpussia kipukohtiin"};
-=======
-import android.widget.TextView;
-
-public class TextViewActivity extends AppCompatActivity {
-    TextView textView;
-    String activityName;
-
-    String[] valmistautuminen = {"Synnyttäjä kokee voimakasta ponnistamisen tarvetta, kun kohdunsuu on täysin avautunut ja sikiön pää on laskeutunut synnytyskanavan alaosaan\n" +
-            "Supistuksia tulee 1-2 minuutin välein ja ne kestävät yleensä minuutin ajan\n" +
-            "\n", "Yleensä lapsen pää eli tarjoutuva osa painaa välilihaa, ja emättimen ulkosuu pullottaa  ja alkaa avautua jolloin lapsen pää saattaa näkyä\n" +
-            "Kun synnytys on edennyt tähän vaiheeseen, niin sitä ei voi enää estää millään tavoin\n" +
-            "Muista toimia rauhallisesti!\n", "Välilihan repeäminen estetään tukemalla sitä toisen kämmenellä, peukalo ja etusormi kurovat välilihaan joustoa ",
-            "Samalla toisella kädellä kontrolloidaan pään syntymistä ","Kiiretilanteessa tukeminen on tärkeämpää kuin aseptiset toimenpiteet.    Pään synnyttyä,"+
-            "se tekee tavallisesti käännöksen niin, että kasvot näyttävät sivulle\n","Pään synnyttyä, hartiat voi auttaa ulos ilman supistusta tai vaihtoehtoisesti synnyttäjän ponnistaessa seuraavan supistuksen aikana ",
-            " Molemmilla käsillä otetaan lapsen pään sivuista kiinni ja painetaan päätä alaspäin, jolloin ylempi hartia syntyy","Seuraavaksi päätä kohotetaan ylöspäin kohti häpyliitosta alemman hartian synnyttämiseksi",
-            "Kun pää ja hartiat ovat syntyneet, molempien käsien etusormet viedään lapsen selän kautta kainaloihin ja lapsen vartalo syntyy hellästi nostamalla","Jos hartiat eivät synny helposti, asetetaan jokin koroke tai tyyny tai vastaava äidin pakaroiden alle"};
-
->>>>>>> 38315e8871f01d4509e2f9b6fb2e2e68b865d1ff
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_view);
 
-<<<<<<< HEAD
         rightArrow = findViewById(R.id.rightArrow);
         leftArrow = findViewById(R.id.leftArrow);
-        TextToChange = findViewById(R.id.textView);
-        homeButton = findViewById(R.id.imageButton);
-        TextToChange.setText(Texts[modifier]);
+        homeButton = findViewById(R.id.homeButton);
+        textView = (TextView) findViewById(R.id.content);
+        textView.setText(valmistautuminen[modifier]);
+
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -90,7 +71,7 @@ public class TextViewActivity extends AppCompatActivity {
                     returnHome(v);
                 }
                 modifier++;
-                TextToChange.setText(Texts[modifier]);
+                textView.setText(valmistautuminen[modifier]);
                 leftArrow.setVisibility(View.VISIBLE);
             }
         });
@@ -102,16 +83,10 @@ public class TextViewActivity extends AppCompatActivity {
                     returnHome(v);
                 }
                 modifier--;
-                TextToChange.setText(Texts[modifier]);
+                textView.setText(valmistautuminen[modifier]);
                 rightArrow.setVisibility(View.VISIBLE);
             }
         });
-    }
-    public void returnHome(View v) {
-        Intent intent = new Intent(this, MenuView.class);
-=======
-        textView = (TextView) findViewById(R.id.content);
-
         Bundle extras = getIntent().getExtras();
         if(extras != null) {
             activityName = extras.getString("ActivityName");
@@ -122,10 +97,9 @@ public class TextViewActivity extends AppCompatActivity {
         }
     }
 
-    public void returnHome(View view) {
+    public void returnHome(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("ActivityName", activityName);
->>>>>>> 38315e8871f01d4509e2f9b6fb2e2e68b865d1ff
         startActivity(intent);
     }
 }
