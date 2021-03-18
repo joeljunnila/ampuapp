34896d2ed195e19feda0eea20c014d6f928aedd4
Joonas <joonas_partanen@hotmail.com>
2021-03-18 15:22:16 +0200

    Useless commit3

--- AmbuApp/app/src/main/java/com/example/ambuapp/MainActivity.java
+++ AmbuApp/app/src/main/java/com/example/ambuapp/MainActivity.java
@@ -1,28 +1,13 @@
 package com.example.ambuapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     String activityName = "Home";
@@ -142,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void erikoistilanteetPage() {
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
