c2d6e20114dfe4f2950b5c94096326e7d1c95c31
Thien <thien_@hotmail.fi>
2021-03-23 14:37:24 +0200

    grant permissions

--- AmbuApp/app/src/main/java/com/example/ambuapp/MenuActivity.java
+++ AmbuApp/app/src/main/java/com/example/ambuapp/MenuActivity.java
@@ -1,8 +1,12 @@
 package com.example.ambuapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -12,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -31,11 +36,21 @@ public class MenuActivity extends AppCompatActivity {
     String activityName = "Home";
     MyFirebase myFirebase;
     Thread myFirebaseThread;
+    boolean once = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        if(once) {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ||
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 1);
+            } else {
+                once = false;
+            }
+        }
 
         homeButton = findViewById(R.id.homeButton);
         title = findViewById(R.id.title);
