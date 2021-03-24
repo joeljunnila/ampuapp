e4ce3eba37d11114360d1fe2d7f3e182946ac7b8
junjols <joel.junnila@gmail.com>
2021-03-24 16:45:25 +0200

    tehty asetukset-osioon palautuminen viime activityyn

--- AmbuApp/app/src/main/java/com/example/ambuapp/Settings.java
+++ AmbuApp/app/src/main/java/com/example/ambuapp/Settings.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,11 +22,14 @@ public class Settings extends AppCompatActivity implements AdapterView.OnItemSel
     TextView title;
     TextView textView;
     TextView textView2;
+    TextView textView3;
     ImageButton leftArrow;
     ImageButton rightArrow;
     String activityName;
     String previousActivityName;
     String[] items = new String[]{"Pieni", "Keskisuuri", "Suuri"};
+
+    public Integer textSize = 20;
 
     MyFirebase myFirebase;
     Thread myFirebaseThread;
@@ -39,6 +43,7 @@ public class Settings extends AppCompatActivity implements AdapterView.OnItemSel
         title = findViewById(R.id.title);
         textView = findViewById(R.id.textView);
         textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
         leftArrow = findViewById(R.id.leftArrow);
         rightArrow = findViewById(R.id.rightArrow);
         rightArrow.setVisibility(View.INVISIBLE);
@@ -59,7 +64,45 @@ public class Settings extends AppCompatActivity implements AdapterView.OnItemSel
         leftArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (previousActivityName.equals("Valmistautuminen1") ||
+                        previousActivityName.equals("Valmistautuminen2") ||
+                        previousActivityName.equals("Valmistautuminen3") ||
+                        previousActivityName.equals("Tarkistus1") ||
+                        previousActivityName.equals("Tarkistus2") ||
+                        previousActivityName.equals("Tarkistus3") ||
+                        previousActivityName.equals("Tarkistus5") ||
+                        previousActivityName.equals("Napanuora1") ||
+                        previousActivityName.equals("Napanuora2") ||
+                        previousActivityName.equals("Napanuora4") ||
+                        previousActivityName.equals("tietoaSovelluksesta")) {
+                    activityName = previousActivityName;
+                    textViewActivity(v);
+                }
+                else if (previousActivityName.equals("Aikana1") ||
+                        previousActivityName.equals("Aikana2") ||
+                        previousActivityName.equals("Aikana3") ||
+                        previousActivityName.equals("Aikana4") ||
+                        previousActivityName.equals("Aikana5") ||
+                        previousActivityName.equals("Aikana6") ||
+                        previousActivityName.equals("Tarkistus4") ||
+                        previousActivityName.equals("Perätila1") ||
+                        previousActivityName.equals("Perätila2") ||
+                        previousActivityName.equals("Perätila3") ||
+                        previousActivityName.equals("Perätila4") ||
+                        previousActivityName.equals("Perätila5") ||
+                        previousActivityName.equals("Hartiadystokia1") ||
+                        previousActivityName.equals("Hartiadystokia2") ||
+                        previousActivityName.equals("Hartiadystokia3") ||
+                        previousActivityName.equals("Hartiadystokia4") ||
+                        previousActivityName.equals("Hartiadystokia5") ||
+                        previousActivityName.equals("Napanuora3")) {
+                    activityName = previousActivityName;
+                    imageTextActivity(v);
+                }
+                else {
+                    activityName = previousActivityName;
+                    menuActivity(v);
+                }
             }
         });
 
@@ -94,6 +137,11 @@ public class Settings extends AppCompatActivity implements AdapterView.OnItemSel
     }
     public void textViewActivity(View view) {
         Intent intent = new Intent(this, TextViewActivity.class);
+        intent.putExtra("ActivityName", activityName);
+        startActivity(intent);
+    }
+    public void imageTextActivity(View view) {
+        Intent intent = new Intent(this, ImageTextActivity.class);
         intent.putExtra("ActivityName", activityName);
         startActivity(intent);
     }
@@ -132,15 +180,26 @@ public class Settings extends AppCompatActivity implements AdapterView.OnItemSel
     public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
         switch (position) {
             case 0:
-                textView2.setTextSize(15);
+                textSize = 10;
+                textView.setTextSize(textSize);
+                textView2.setTextSize(textSize);
+                textView3.setTextSize(textSize);
+
                 // Whatever you want to happen when the first item gets selected
                 break;
             case 1:
-                textView2.setTextSize(20);
+                textSize = 20;
+                textView.setTextSize(textSize);
+                textView2.setTextSize(textSize);
+                textView3.setTextSize(textSize);
+
                 // Whatever you want to happen when the second item gets selected
                 break;
             case 2:
-                textView2.setTextSize(40);
+                textSize = 40;
+                textView.setTextSize(textSize);
+                textView2.setTextSize(textSize);
+                textView3.setTextSize(textSize);
                 // Whatever you want to happen when the thrid item gets selected
                 break;
         }
@@ -150,4 +209,5 @@ public class Settings extends AppCompatActivity implements AdapterView.OnItemSel
     public void onNothingSelected(AdapterView<?> parent) {
         // TODO Auto-generated method stub
     }
+
 }
