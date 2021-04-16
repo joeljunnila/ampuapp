d3683fbe5baafe10414ff4cb12a81f72e49d2568
Thien <thien_@hotmail.fi>
2021-04-16 09:00:17 +0300

    javascript toteuttaa koodin switch casesta vaikkei sen pidä

--- AmbuApp Website/public/scripts.js
+++ AmbuApp Website/public/scripts.js
@@ -1,9 +1,37 @@
 function changeContent(page) {
-    if(page == "kotisivu") {
-        document.getElementById("menuLayout").style.display = "block";
-        document.getElementById("imageTextLayout").style.display = "none";
-    } else {
-        document.getElementById("menuLayout").style.display = "none";
-        document.getElementById("imageTextLayout").style.display = "block";
+    switch(page) {
+        case "kotisivu":
+            document.getElementById("menuLayout").style.display = "block";
+            document.getElementById("imageTextLayout").style.display = "none";
+            document.getElementById("button4").style.visibility = "visible"; // kussee
+
+            document.getElementById("button1").onclick = changeContent("erikoistilanteetSivu"); //kussee
+
+            document.getElementById("button1").textContent = "Valmistautuminen";
+            document.getElementById("button2").textContent = "Synnytysvaiheet";
+            document.getElementById("button3").textContent = "Synnytyksen jälkeen";
+            document.getElementById("button4").textContent = "Erikoistilanteet";
+            break;
+        case "valmistautuminenSivu1":
+            document.getElementById("menuLayout").style.display = "none";
+            document.getElementById("imageTextLayout").style.display = "block";
+            break;
+        case "synnytysvaiheetSivu1":
+            document.getElementById("menuLayout").style.display = "none";
+            document.getElementById("imageTextLayout").style.display = "block";
+            break;
+        case "synnytyksenJalkeenSivu1":
+            document.getElementById("menuLayout").style.display = "none";
+            document.getElementById("imageTextLayout").style.display = "block";
+            break;
+        case "erikoistilanteetSivu":
+            document.getElementById("menuLayout").style.display = "block";
+            document.getElementById("imageTextLayout").style.display = "none";
+            document.getElementById("button4").style.visibility = "hidden";
+
+            document.getElementById("button1").textContent = "Perätila";
+            document.getElementById("button2").textContent = "Hartiadystokia";
+            document.getElementById("button3").textContent = "Napanuoran esiinluiskahdus";
+            break;
     }
 }
